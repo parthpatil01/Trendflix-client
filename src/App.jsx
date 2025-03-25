bc5f19d556557f7de2c7fa8443a4c3d9463163a5
parthpatil01 <patilvparth15@gmail.com>
2025-03-25 17:04:06 -0300

    Add @vercel/analytics package and integrate analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login'
 import Details from './pages/Details';
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   
@@ -13,11 +13,12 @@ function App() {
     <Router>
       <div >
         <Routes>
-          <Route path="/" element={<Homepage />} />s
+          <Route path="/" element={<Homepage />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/sign-in" element={<Login />} />
           <Route path="/details" element={<Details />} />
         </Routes>
+        <Analytics />
       </div>
 
     </Router>
